06fc0e0348b9a861b16dbfe6c64c9c68573d2857
Bruno99 <maks.udachin26@gmail.com>
2016-06-20 19:02:16 +0400

    Начало сырого кода...!
    Добавлена конструкция, которая будет переделан в классы и т.п.!

--- main.py
+++ main.py
@@ -1,1 +1,33 @@
-print("PureBot by mcg76&Bruno99")
+
+# Импортирование модулей
+
+from random import randint
+
+# Глобальные переменные
+
+global not_found
+global prefix
+
+not_found = "Команда не найдена"
+prefix = "[PureBot]"
+
+# Command «/help»
+
+def help(commandsend):
+ if commandsend == "/help":
+  print("Список комманд:\n/help - Помощь по боту\n/random - Случайное число от 0 до 100")
+ else:
+  print(not_found)
+  
+# Command «/random»
+  
+def random(commandsend):
+ if commandsend == "/random":
+  random = randint(0,100)
+  print("\nСлучайное число: ",random)
+ else:
+  print(not_found)
+   
+commands = help,random
+
+# Код сырой, не рекомендуется что-либо делать дальше...!!!
