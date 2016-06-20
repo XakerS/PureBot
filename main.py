3a1baae4e4e997476e3e285fccac6cb102f99703
Bruno99 <maks.udachin26@gmail.com>
2016-06-21 00:05:01 +0400

    Обновление 0.2
    Потихоньку добавляем мини игру «True or False».
    Пофикшены и сделаны некоторые детали..

--- main.py
+++ main.py
@@ -1,26 +1,30 @@
+# coding: utf-8
 # Импортирование модулей
 
 from random import randint
+from random import choice
 
 # Глобальные переменные
 
 global not_found
 global prefix
+global inp
 
-not_found = "Команда не найдена"
+not_found = "Команда не найдена, введите «/help» для просмотра доступных комманд."
 prefix = "[PureBot]"
 
 # Начало программы
 
 print("Запуск программы...\nПрограмма «PureBot» предназначена для лиц старше 14+  :D")
-print("Разработчики:\n Xaker Su - mcg76\n Максим Удачин - Bruno99\n\nВерсия: 0.1")
+print("Разработчики:\n Xaker Su - mcg76\n Максим Удачин - Bruno99\n\nВерсия: 0.2")
 print("-"*50,"\n")
+inp = str(input("Введите комманду: "))
 
 # Command «/help»
 
 def helps(commandsend):
  if commandsend == "/help":
-  print("\n Список комманд:\n/help - Помощь по боту\n/random - Случайное число от 0 до 100")
+  print("\n Список комманд:\n/help - Помощь по боту\n/random - Случайное число от 0 до 100\n/tf - Мини игра «True or False»")
 
 # Command «/random»
   
@@ -29,24 +33,43 @@ def random(commandsend):
   random = randint(0,100)
   print("\n Случайное число: ",random)
 
-# Мини игра Ture or False
+# Мини игра True or False
 
 def trueorfalse(commandsend):
- if commandsend == "/trueorfalse":
-  words = {True:"Твоя мамка бухала?"}
-  randwords = choice([words[key]])
+ if commandsend == "/tf":
+  words = {
+  	1:"Россия - Это молодая страна?",
+  	1:"Test aaaaaaa?",
+  	2:"Тестовый текст?"
+  	} 
+  ri = randint(1,2)
+  randwords = choice([words[ri]]) 
+  print("\n",randwords)
+  Да = "1"
+  да = "1"
+  Нет = "2"
+  нет = "2"
+  result = input("Выберите ответ «Да» или «Нет»?\nОтвет: ")
+  if result == "1":
+   print("\nИ это...\nПравильный ответ!")
+   #print("\nВаша награда: ",randmoney)
+  else:
+   print("Неверный ответ!")
    
-#commands = helps,random,trueorfalse
+# commands = helps,random,trueorfalse
+# Функция ввода и вывода
 
-inp = str(input("Введите комманду: "))
-if inp == "/help":
- helps("/help")
-elif inp == "/random":
- random("/random")
-elif inp == "/trueorfalse":
- random("/trueorfalse")
-else:
- print(not_found)
+def entry(inp):
+ if inp == "/help":
+  helps(inp)
+ elif inp == "/random":
+  random(inp)
+ elif inp == "/tf":
+  trueorfalse(inp)
+ else:
+  print(not_found)
+ 
+entry(inp)
 
 # Конец программы
 
